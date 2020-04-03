e0661d6d6821fc35a37fbd74fbf3ff05ec0cfba2
luky1300 <jstrelnikova@gmail.com>
2020-04-02 20:25:52 -0400

    notifications about ontime and late tracks refactored

--- toTrackers.js
+++ toTrackers.js
@@ -1,4 +1,5 @@
 const {firestore} = require('./config');
+var Distance = require('geo-distance');
 
 //send push notification to tokens
 //set timer to run function every xx min
@@ -19,14 +20,14 @@ sendPushToTrackers = function () {
     body: {
       "to": token.pushToken,
       "sound": "default",
-      "body": `Hey! Your friend late to her/his destination for ${token.howLate}.`
+      "body": token.message
     },
     json: true,
     function(error, response, body){
       // resp.send(body)
     }
   })
-  console.log(`"Late notification" sent to traker: ${token.pushToken} - late for ${token.howLate}`)
+  console.log(`"Track notifications" sent to : ${token.pushToken}`)
 })
 console.log(`Next batch will be sent in ${timeout/1000} sec`)
 })
@@ -49,7 +50,7 @@ const getUserPushTokens = async () => {
       if (token) {
       pushTokensArr.push({
         pushToken: token,
-        howLate: uidArr[i].howLate
+        message: uidArr[i].message
       })
     }}
     console.log('got all late tokens: ', pushTokensArr)
@@ -63,31 +64,77 @@ const getUserPushTokens = async () => {
 //1. Get all tracks we need to send notifications about
 const getLateUsers = async () => {
   try {
-    let homeLate = []
+    let usersToNotify = []
     //get all tracks with status "open"
     let tracks = await firestore
     .collection('tracks')
     .where('status', '==', 'open')
     .get()
-    tracks.forEach(track => {
+    tracks.forEach(async track => {
       //check if current time more then ETA
-      let time = track.data().ETA
+      let trackData = track.data()
+      let time = trackData.ETA
       let howLate = Date.now()/1000 - time.seconds
+      let trackers = track.data().tracker
       // add tracker's uid and late time for all open tracks to the array
-      if (howLate > 0) {
-        let trackers = track.data().tracker
+      if (howLate > 0 && !trackData.lateTrackNotification) {
         if (trackers) {
-        // console.log('trackers', trackers)
+
+        //calculate, how far trackee from their final destination
+        let howFar = 0
+        if (trackData.finalLocation) {
+        let current = {
+          lat: trackData.currentLocation.latitude,
+          lon: trackData.currentLocation.longitude
+        }
+        let final = {
+          lat: trackData.finalLocation.latitude,
+          lon: trackData.finalLocation.longitude
+        }
+        howFar = Distance.between(current, final).human_readable().distance
+        }
+
+        if (howFar > 0.2) {
         trackers.forEach(tracker => {
-          homeLate.push({
+          usersToNotify.push({
             uid: tracker,
-            howLate: `about ${Math.floor((howLate/1000/60) << 0)} min`
+            message: `Your trackee about ${howFar} km away from you final destination.`
           })
         })
+        usersToNotify.push({
+          uid: trackData.trackee.id,
+            message: `You are about ${howFar} km away from you final destination.`
+        })
+        await firestore
+          .collection('tracks')
+          .doc(track.id)
+          .update({
+            lateTrackNotification: 'sent'
+          })
+      } else {
+        trackers.forEach(async tracker => {
+          usersToNotify.push({
+            uid: tracker,
+            message: `Your trackee is safe to they destination.`
+          })
+        })
+        usersToNotify.push({
+          uid: trackData.trackee,
+            message: `You are safe at your destination. You trip is completed`
+        })
+        await firestore
+          .collection('tracks')
+          .doc(track.id)
+          .update({
+            status: 'closed'
+          })
+      }
+
+
       }}
     })
-    console.log('got all trackers for all late open tracks: ', homeLate)
-    return homeLate;
+    console.log('got all users to notify about open tracks: ', usersToNotify)
+    return usersToNotify;
   } catch (e) {
     console.error(e)
   }
